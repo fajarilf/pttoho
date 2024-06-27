e6f7f8f39e76df8fe2e7be4cf8c6c7c4ba83eeca
fajarilf <fjr.ilf.firmansyah@gmail.com>
2024-06-27 20:49:00 +0700

    done

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use fajarilham\App\Router;
 use fajarilham\Controller\MainController;
 
 Router::add("GET", "/", MainController::class, "index");
+Router::add("GET", "/data", MainController::class, "getData");
 Router::add("GET", "/table", MainController::class, "table");
+Router::add("GET", "/table/download", MainController::class, "downloadData");
 
 Router::run();
